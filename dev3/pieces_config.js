07dd7cdafe76701732df07ec26f472ed2a43f013
yu <yy1973100@163.com>
2026-04-18 10:50:31 +0800

    update dev3
    上传了dev3的未完成版本

---
+++ dev3/pieces_config.js
@@ -0,0 +1,15 @@
+// 棋子配置文件
+var piecesConfig = {
+  "pieces": [
+    {"id": 0, "name": "e_1", "color": "red",   "reaction": 85, "shooting": 50},
+    {"id": 1, "name": "e_2", "color": "red",   "reaction": 60, "shooting": 50},
+    {"id": 2, "name": "e_3", "color": "red",   "reaction": 90, "shooting": 50},
+    {"id": 3, "name": "e_4", "color": "red",   "reaction": 70, "shooting": 50},
+    {"id": 4, "name": "e_5", "color": "red",   "reaction": 80, "shooting": 50},
+    {"id": 5, "name": "f_1", "color": "green", "reaction": 75, "shooting": 50},
+    {"id": 6, "name": "f_2", "color": "green", "reaction": 55, "shooting": 50},
+    {"id": 7, "name": "f_3", "color": "green", "reaction": 65, "shooting": 50},
+    {"id": 8, "name": "f_4", "color": "green", "reaction": 45, "shooting": 50},
+    {"id": 9, "name": "f_5", "color": "green", "reaction": 50, "shooting": 50}
+  ]
+};
