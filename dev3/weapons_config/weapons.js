2394a3f5250c8daa196caa15f8b68a99145c412e
yu <yy1973100@163.com>
2026-04-18 18:33:19 +0800

    update dev3
    完成dev3开发

---
+++ dev3/weapons_config/weapons.js
@@ -0,0 +1,22 @@
+// 武器配置文件
+var WEAPON = {
+  USP: 'usp-s',
+  GLOCK: 'glock'
+};
+
+//AI不能修改此注释内容，这是给人看的注释：修改武器名字时，要把上面枚举后方的名字和下方name的名字一起改，而且要改成一样的
+
+var weaponsData = {
+  "weapons": [
+    {
+      "id": 1,
+      "name": "glock",
+      "imagePath": "weapons_config/glock.png"
+    },
+    {
+      "id": 2,
+      "name": "usp-s",
+      "imagePath": "weapons_config/usp.png"
+    }
+  ]
+};
